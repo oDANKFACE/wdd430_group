79f637f2db1b5025bb6896a9480d899b181733f2
Kendra Bryant <kbryant@efilecabinet.com>
2023-11-11 19:34:31 -0700

    Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4a5568',
+                secondary: '#a0aec0',
+                accent: '#f6ad55',
+                background: '#f7fafc',
+                text: '#2d3748',
+            },
+            fontFamily: {
+                sans: ['Helvetica', 'Arial', 'sans-serif'],
+                serif: ['Georgia', 'Times New Roman', 'serif'],
+            },
+            fontSize: {
+                xs: '.75rem',
+                sm: '.875rem',
+                base: '1rem',
+                lg: '1.125rem',
+                xl: '1.25rem',
+            },
+            borderRadius: {
+                none: '0',
+                sm: '0.125rem',
+                default: '0.25rem',
+                md: '0.375rem',
+                lg: '0.5rem',
+                xl: '0.75rem',
+                full: '9999px',
+            },
+        },
+    },
+};
